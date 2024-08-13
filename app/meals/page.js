8009e6b1919fb4758b027e13d2a7f0d766eb4189
Surendra Yalakala <surendrayalakala@gmail.com>
2024-08-13 15:16:59 +0530

    meta data added

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import Link from "next/link";
 import { Suspense } from "react";
 import classes from "./page.module.css";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community",
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
